a6d58077a53b4d40322b6583f4725457b2cdc194
verzcar <carlo.verzeri@vecomentman.de>
2022-09-04 09:30:06 +0200

    adapted migration of global circle

--- repository/migrations/000002_init_circles_data.up.sql
+++ repository/migrations/000002_init_circles_data.up.sql
@@ -1,6 +1,11 @@
 BEGIN;
 
 INSERT INTO circles(name, description, image_src, created_from, created_at, updated_at)
-VALUES ('Global', '', '', 'passoAvanti', now(), now());
+VALUES ('Global',
+        'One of the most impressive circles around the globe',
+        '',
+        'passoAvanti',
+        now(),
+        now());
 
 COMMIT;
