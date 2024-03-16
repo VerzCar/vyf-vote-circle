6052b7435ce96acc96ee032110eef96840ee5a55
verzcar <carlo.verzeri@vecomentman.de>
2024-03-16 14:02:08 +0100

    added cors

--- app/router/router.go
+++ app/router/router.go
@@ -4,6 +4,7 @@ import (
 	"github.com/VerzCar/vyf-vote-circle/app/config"
 	"github.com/gin-gonic/gin"
 	cors "github.com/rs/cors/wrapper/gin"
+	"net/http"
 )
 
 // Setup the router
@@ -22,11 +23,18 @@ func Setup(environment string) *gin.Engine {
 // AddMiddleware adds the available middlewares to the router
 func addMiddleware(r *gin.Engine) {
 	corsOptions := cors.Options{
-		AllowedOrigins: []string{"https://vyf-web-app-c3f1d65ba31f.herokuapp.com"},
-		AllowOriginFunc: func(origin string) bool {
-			return origin == "https://vyf-web-app-c3f1d65ba31f.herokuapp.com"
+		AllowedOrigins: []string{
+			"https://vyf-web-app-c3f1d65ba31f.herokuapp.com",
+			"https://vyf-web-app-c3f1d65ba31f.herokuapp.com/",
 		},
-		AllowedMethods:   []string{"GET", "POST", "OPTION", "DELETE", "PUT", "PATCH"},
+		AllowedMethods: []string{
+			http.MethodHead,
+			http.MethodGet,
+			http.MethodPost,
+			http.MethodPut,
+			http.MethodPatch,
+			http.MethodDelete,
+		},
 		AllowedHeaders:   []string{"Origin"},
 		ExposedHeaders:   []string{"Content-Length"},
 		MaxAge:           10800, // 3 hours
